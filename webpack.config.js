421754675e58b1fe1401f27f2f7d1d1622bd3b1c
OBKoro1 <1677593011@qq.com>
2019-07-10 20:59:43 +0800

    1.3.1

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,15 @@
  * @Author: OBKoro1
  * @Date: 2019-07-09 20:35:45
  * @LastEditors: OBKoro1
- * @LastEditTime: 2019-07-10 20:00:48
+ * @LastEditTime: 2019-07-10 20:57:44
  * @Description: webpack 打包配置
  */
+const path = require('path');
+
 module.exports = {
+    output: {
+        globalObject: 'this', // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况
+    },
     node: {
         fs: 'empty',
         net: 'empty',
